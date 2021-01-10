d5151d626ae706292e63e4b0c6d03afd1143d317
debamitra <debamitra.mukherjee@gmail.com>
2021-01-10 20:29:09 +0530

    changed filtercomponent styling to react-boot-strap

--- src/FilterSearchComponent.js
+++ src/FilterSearchComponent.js
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 
 
 import { colourOptions } from './data';
@@ -71,16 +71,16 @@ const FilterSearchComponent = ({ handleSortBy, handleSearchResult }) => {
     const addEvent = event => {
         console.log("state ", state);
         event.preventDefault();
-     
+
         axios
             .post('/search', { ...state })
             .then(response => {
                 setResult(response.data);
-         
+
                 console.log("resp: ", response.data);
-               
+
                 handleSearchResult(response.data);
-                
+
 
             })
             .catch(() => {
@@ -136,18 +136,18 @@ const FilterSearchComponent = ({ handleSortBy, handleSearchResult }) => {
     return (
         <div>
             <Form onSubmit={addEvent}>
-                <div className="container-lg">
-                    <div className="row">
-                        Sort by :
-                        <div className="col-md-2">
+                <Container fluid={"sm"} >
+                    <Row >
+                        Sort by : 
+                        <Col className="with-margin" xs={12} md={3}>
                             <Select placeholder="Sort by:" options={[{ label: "upcoming events", value: 46 },
                             { label: "recently submitted events", value: 58 }]}
                                 value={state.selectedOptionSortBy}
                                 onChange={handleChangeSortBy}
                             />
-                        </div>
+                        </Col>
 
-                        <div className="col-md-4">
+                        <Col className="with-margin" xs={12} md={4}>
                             <Select
                                 //defaultValue={[colourOptions[2], colourOptions[3]]}
                                 isMulti
@@ -158,9 +158,9 @@ const FilterSearchComponent = ({ handleSortBy, handleSearchResult }) => {
                                 className="basic-multi-select"
                                 classNamePrefix="select"
                             />
-                        </div>
+                        </Col>
 
-                        <div className="col-md-3 buttonIn">
+                        <Col className="with-margin" xs={12} md={3}>
 
                             <DateRangePicker
                                 initialSettings={{ singleDatePicker: true, autoApply: true }}
@@ -171,7 +171,9 @@ const FilterSearchComponent = ({ handleSortBy, handleSearchResult }) => {
                                 <input type="text" className="form-control" />
 
                             </DateRangePicker>
-                            {/*
+
+                        </Col>
+                        {/*
                             <DateRangePicker
                                 initialSettings={{ 
                                     autoUpdateInput: true,
@@ -186,17 +188,18 @@ const FilterSearchComponent = ({ handleSortBy, handleSearchResult }) => {
                             </DateRangePicker>
                             <div aria-hidden="true" id="bu" class="select__indicator select__clear-indicator css-tlfecz-indicatorContainer"><svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" class="css-6q0nyr-Svg"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg></div>
                                 */
-                            }
-                        </div>
+                        }
 
-                        <div className="col-md-2">
+
+                        <Col className="with-margin" xs={12} md={1}>
                             <Button variant="primary" type="submit">
                                 Submit
                             </Button>
 
-                        </div>
-                    </div>
-                </div>
+                        </Col>
+
+                    </Row>
+                </Container>
 
             </Form>
 
